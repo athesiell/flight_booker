ae961fa9ba6335483d351152c6f8a6cb6b6bdabf
Aidar <ar.ibraev98@gmail.com>
2024-02-25 20:11:39 +0600

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "flights#index"
   resources :flights
+  resources :bookings, only: [:new, :create, :show]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
