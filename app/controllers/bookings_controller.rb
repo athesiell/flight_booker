5e3df8a2f1b466e373e344840740bdabb00ec660
Aidar <ar.ibraev98@gmail.com>
2024-02-26 20:14:10 +0600

    Create booking methods

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,29 @@
 class BookingsController < ApplicationController
   def new
+    @flight = Flight.find(params[:flight_id])
+    @booking = Booking.new
+
+    params[:passengers].to_i.times { @booking.passengers.build }
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
+    @flight = Flight.find(@booking.flight_id)
+    @passengers = @booking.passengers
+  end
+
+  private 
+
+  def booking_params
+    params.require(:booking).permit(:flight_id, passengers_attributes: [:name, :email])
   end
 end
